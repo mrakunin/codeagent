e9c9aa41e03fe61e942aa468324c4a0a07baa150
Mr. AkuNin <mrakunin@gmx.de>
2026-01-08 10:55:55 +0100

    first request

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
-def main():
-    print("Hello from codeagent!")
+import os
+from dotenv import load_dotenv
+from google import genai
 
+llm_model = "gemini-2.5-flash"
+llm_request = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+
+def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if api_key is None:
+        raise RuntimeError("Could not read GEMINI_API_KEY")
+
+    client = genai.Client(api_key=api_key)
+
+    response = client.models.generate_content(model=llm_model, contents=llm_request)
+    print(response.text)
 
 if __name__ == "__main__":
     main()
