bd92ca382976d4a3768e6b57fc88b200935edaa8
Mr. AkuNin <mrakunin@gmx.de>
2026-01-09 12:00:20 +0100

    added --verbose argument

--- main.py
+++ main.py
@@ -18,15 +18,19 @@ def main():
 
     parser = argparse.ArgumentParser(description="AI agent for code generation")
     parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
 
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
     response = client.models.generate_content(model=llm_model, contents=messages)
     if response.usage_metadata is None:
         raise RuntimeError("No usage metadate: probebly failed API request")
-    print(
-        f"Prompt tokens: {response.usage_metadata.prompt_token_count}\nResponse tokens: {response.usage_metadata.candidates_token_count}"
-    )
+
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
     print(response.text)
 
 
