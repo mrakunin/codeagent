7081784c974b98c9282255ea677c4b3969d89f5a
Mr. AkuNin <mrakunin@gmx.de>
2026-01-09 10:20:01 +0100

    added error checking

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from google import genai
 llm_model = "gemini-2.5-flash"
 llm_request = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
 
+
 def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
@@ -14,7 +15,13 @@ def main():
     client = genai.Client(api_key=api_key)
 
     response = client.models.generate_content(model=llm_model, contents=llm_request)
+    if response.usage_metadata is None:
+        raise RuntimeError("No usage metadate: probebly failed API request")
+    print(
+        f"Prompt tokens: {response.usage_metadata.prompt_token_count}\nResponse tokens: {response.usage_metadata.candidates_token_count}"
+    )
     print(response.text)
+
 
 if __name__ == "__main__":
     main()
