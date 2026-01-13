f4e44b5eff0061ea2458af1cbd1c89de12107767
Mr. AkuNin <mrakunin@gmx.de>
2026-01-13 18:35:45 +0100

    implemented main loop

--- main.py
+++ main.py
@@ -23,40 +23,56 @@ def main():
     args = parser.parse_args()
 
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
-    response = client.models.generate_content(
-        model=llm_model,
-        contents=messages,
-        config=types.GenerateContentConfig(
-            tools=[available_functions], system_instruction=system_prompt, temperature=0
-        ),
-    )
-    if response.usage_metadata is None:
-        raise RuntimeError("No usage metadata: probebly failed API request")
 
-    if args.verbose:
-        print(f"User prompt: {args.user_prompt}")
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    for _ in range(20):
+        response = client.models.generate_content(
+            model=llm_model,
+            contents=messages,
+            config=types.GenerateContentConfig(
+                tools=[available_functions],
+                system_instruction=system_prompt,
+                temperature=0,
+            ),
+        )
+        if args.verbose:
+            print(f"User prompt: {args.user_prompt}")
+            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
-    function_results = []
-    # print functions calls if available
-    if response.function_calls is None:
-        print(response.text)
-    else:
-        for call in response.function_calls:
-            result = call_function(call)
+        if response.usage_metadata is None:
+            raise RuntimeError("No usage metadata: probebly failed API request")
 
-            if result.parts is None:
-                raise Exception("Error: parts field is missing")
-            if result.parts[0].function_response is None:
-                raise Exception("Error: function_response field is missing")
-            if result.parts[0].function_response.response is None:
-                raise Exception("Error: response field is missing")
+        for candidate in response.candidates:
+            messages.append(candidate.content)
 
-            function_results += result.parts[0]
+        # print functions calls if available
+        if response.function_calls is None:
+            print(response.text)
+            return
+        else:
+            function_results = []
 
-            if args.verbose:
-                print(f"-> {result.parts[0].function_response.response}")
+            for call in response.function_calls:
+                result = call_function(call)
+
+                if result.parts is None:
+                    raise Exception("Error: parts field is missing")
+                if result.parts[0].function_response is None:
+                    raise Exception("Error: function_response field is missing")
+                if result.parts[0].function_response.response is None:
+                    raise Exception("Error: response field is missing")
+
+                function_results.append(result.parts[0])
+                print(function_results)
+
+                if args.verbose:
+                    print(f"-> {result.parts[0].function_response.response}")
+
+            messages.append(types.Content(role="user", parts=function_results))
+
+    # we reached the maximum iteration count
+    print("Error: Reched the maximum number of iterations")
+    exit(1)
 
 
 if __name__ == "__main__":
